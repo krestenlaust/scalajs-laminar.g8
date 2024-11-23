1ddd99dc2450f319051492428d3267ddc19fffaf
Kresten Laust <krestenlaust@gmail.com>
2024-11-23 16:05:19 +0100

    Make laminar/scalajs demo

--- src/main/g8/src/main/scala/Main.scala
+++ src/main/g8/src/main/scala/Main.scala
@@ -1,5 +1,18 @@
+import com.raquo.laminar.api.L.{*, given}
+import org.scalajs.dom
+
 @main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+  // Laminar initialization
+  renderOnDomContentLoaded(dom.document.querySelector("#app"), appElement())
+
+def appElement(): HtmlElement =
+  div(
+    h1("Hello Vite!"),
+    h2(msg),
+    ul(
+      li("Item 1"),
+      li("Item 2"),
+    ),
+  )
 
 def msg = "I was compiled by Scala 3. :)"
